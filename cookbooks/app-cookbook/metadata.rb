e38c9d104eca76508a9f79d8d71ffe781be9d8b1
ian.grant <tnargnai@gmail.com>
2016-08-18 16:19:09 +0100

    version bump.

--- cookbooks/app-cookbook/metadata.rb
+++ cookbooks/app-cookbook/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ian.grant@barclays.com'
 license 'all_rights'
 description 'Installs/Configures app-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.3'
+version '0.1.4'
 
 depends 'b-nginx', '>= 0.1.1'
 depends 'b-goapp', '>= 0.1.0'
