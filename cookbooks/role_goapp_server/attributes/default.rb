409f738f7728910c6f9e41706343ad84c8b59464
ian.grant <tnargnai@gmail.com>
2016-10-04 16:43:39 +0100

    Added user to run goapp

--- cookbooks/role_goapp_server/attributes/default.rb
+++ cookbooks/role_goapp_server/attributes/default.rb
@@ -2,8 +2,12 @@ case node.chef_environment
 when 'local', 'docker'
   # attributes
 when 'test'
-  # attributes
+  default['b-goapp']['webapp']['user'] = 'ubuntu'
+  default['b-goapp']['webapp']['group'] = 'ubuntu'
 when 'uat'
-  # attributes
+  default['b-goapp']['webapp']['user'] = 'ubuntu'
+  default['b-goapp']['webapp']['group'] = 'ubuntu'
 when 'prd'
+  default['b-goapp']['webapp']['user'] = 'ubuntu'
+  default['b-goapp']['webapp']['group'] = 'ubuntu'
 end
