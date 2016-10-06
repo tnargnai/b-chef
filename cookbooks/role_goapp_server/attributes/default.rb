77203b9cbc746c4c7a8c12aaa8271a479b4eced2
ian.grant <tnargnai@gmail.com>
2016-10-06 16:42:21 +0100

    Changed the user to vagrant for the goapp server

--- cookbooks/role_goapp_server/attributes/default.rb
+++ cookbooks/role_goapp_server/attributes/default.rb
@@ -2,12 +2,12 @@ case node.chef_environment
 when 'local', 'docker'
   # attributes
 when 'test'
-  default['b-goapp']['webapp']['user'] = 'ubuntu'
-  default['b-goapp']['webapp']['group'] = 'ubuntu'
+  default['b-goapp']['webapp']['user'] = 'vagrant'
+  default['b-goapp']['webapp']['group'] = 'vagrant'
 when 'uat'
-  default['b-goapp']['webapp']['user'] = 'ubuntu'
-  default['b-goapp']['webapp']['group'] = 'ubuntu'
+  default['b-goapp']['webapp']['user'] = 'vagrant'
+  default['b-goapp']['webapp']['group'] = 'vagrant'
 when 'prd'
-  default['b-goapp']['webapp']['user'] = 'ubuntu'
-  default['b-goapp']['webapp']['group'] = 'ubuntu'
+  default['b-goapp']['webapp']['user'] = 'vagrant'
+  default['b-goapp']['webapp']['group'] = 'vagrant'
 end
